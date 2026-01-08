46e4a83e678a15af05369c42b0b14a7240bfb91e
Kseniia <kore-ksenia@yandex.ru>
2026-01-09 00:37:31 +0300

    неправильно лежал главный класс event service

--- core/event-service/src/main/java/practicum/EventServiceApplication.java
+++ core/event-service/src/main/java/practicum/EventServiceApplication.java
@@ -1,8 +1,9 @@
+package practicum;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import practicum.StatsClient;
 import practicum.client.RequestClient;
 import practicum.client.UserClient;
 
