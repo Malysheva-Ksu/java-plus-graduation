22bb3f1fbca02b3d0d39c062fc2e66e3855b48e6
Kseniia <kore-ksenia@yandex.ru>
2026-01-09 00:23:51 +0300

    правки application

--- core/event-service/src/main/java/EventServiceApplication.java
+++ core/event-service/src/main/java/EventServiceApplication.java
@@ -2,9 +2,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import practicum.StatsClient;
 import practicum.client.RequestClient;
 import practicum.client.UserClient;
