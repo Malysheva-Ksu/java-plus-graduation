de69024035a9f512fb692b3e670ecebb28f17998
Kseniia <kore-ksenia@yandex.ru>
2026-01-14 01:39:42 +0300

    добавила сканирование сущностей во всех модулях

--- core/event-service/src/main/java/practicum/EventServiceApplication.java
+++ core/event-service/src/main/java/practicum/EventServiceApplication.java
@@ -2,11 +2,15 @@ package practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import practicum.client.RequestClient;
 import practicum.client.UserClient;
 
 @SpringBootApplication
+@EntityScan(basePackages = "practicum")
+@EnableJpaRepositories(basePackages = "practicum")
 @EnableFeignClients(clients = {
         UserClient.class,
         RequestClient.class
