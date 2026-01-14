0106869ef6601118cbe502c29410289230efb023
Kseniia <kore-ksenia@yandex.ru>
2026-01-14 09:40:59 +0300

    правки

--- core/event-service/src/main/java/practicum/EventServiceApplication.java
+++ core/event-service/src/main/java/practicum/EventServiceApplication.java
@@ -11,7 +11,7 @@ import practicum.client.UserClient;
 import java.util.stream.Stream;
 
 @SpringBootApplication
-@EntityScan
+@EntityScan("practicum.model")
 @EnableJpaRepositories
 @EnableFeignClients(clients = {
         UserClient.class,
